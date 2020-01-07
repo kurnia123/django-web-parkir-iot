a87c82a9dbb93d375266c5ad3051b71b19e6a9bf
rizky kurnia <39643762+kurnia123@users.noreply.github.com>
2020-01-07 20:21:45 +0700

    1

--- parkir/views.py
+++ parkir/views.py
@@ -58,9 +58,12 @@ def updateDBBooking(request,idParkir):
 def updateDBKosong(request,idParkir):
 	user = User.objects.get(username=request.user)
 	data = tempatParkir.objects.get(id_parkir=idParkir)
+	databooking = Booking.objects.get(booking=data.booking.pk)
+	databooking.delete()
+	databooking.save()
+	
 	data.status = 'kosong'
 	data.user = None
-
 	data.save()
 
 	return redirect("/parkir/")

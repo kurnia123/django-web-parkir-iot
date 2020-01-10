0f7f604406e49778af06da9268597db501c39407
kurnia <rizkykurnia219@gmail.com>
2020-01-10 18:11:38 +0700

    kurnia

--- parkir/admin.py
+++ parkir/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
+
+class tempatParkirAdmin(admin.ModelAdmin):
+	readonly_fields = [
+		'user',
+	]
+
 admin.site.register(Booking)
-admin.site.register(tempatParkir)
+admin.site.register(tempatParkir,tempatParkirAdmin)
